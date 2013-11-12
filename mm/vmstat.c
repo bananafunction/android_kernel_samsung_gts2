918b43a3883345363bacadfb88a9b2d5e82e71bc
Toshi Kani <toshi.kani@hp.com>
2013-11-12 15:08:12 -0800

    mm: set N_CPU to node_states during boot
    After a system booted, N_CPU is not set to any node as has_cpu shows an
    empty line.
    
      # cat /sys/devices/system/node/has_cpu
      (show-empty-line)
    
    setup_vmstat() registers its CPU notifier callback,
    vmstat_cpuup_callback(), which marks N_CPU to a node when a CPU is put
    into online.  However, setup_vmstat() is called after all CPUs are
    launched in the boot sequence.
    
    Changed setup_vmstat() to mark N_CPU to the nodes with online CPUs at
    boot, which is consistent with other operations in
    vmstat_cpuup_callback(), i.e.  start_cpu_timer() and
    refresh_zone_stat_thresholds().
    
    Also added get_online_cpus() to protect the for_each_online_cpu() loop.
    
    Signed-off-by: Toshi Kani <toshi.kani@hp.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Reviewed-by: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Tested-by: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: I39d7c3196617505b2f330ab64a099b658fc5c2ac
    
     Conflicts:
    	mm/vmstat.c
    
    Change-Id: I7c2ca39b14779d7bac72894de52cd97490066bfc

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1287,8 +1287,10 @@ static int __init setup_vmstat(void)
 
 	register_cpu_notifier(&vmstat_notifier);
 
-	for_each_online_cpu(cpu)
+	for_each_online_cpu(cpu) {
 		start_cpu_timer(cpu);
+		node_set_state(cpu_to_node(cpu), N_CPU);
+	}
 #endif
 #ifdef CONFIG_PROC_FS
 	proc_create("buddyinfo", S_IRUGO, NULL, &fragmentation_file_operations);
