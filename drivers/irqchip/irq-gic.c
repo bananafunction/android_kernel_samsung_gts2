3c331105d38d7bd9f53bd1255130df0dc24ba760
Thomas Gleixner <tglx@linutronix.de>
2014-04-16 14:36:44 +0000

    irqchip: Gic: Support forced affinity setting
    commit ffde1de64012c406dfdda8690918248b472f24e4 upstream.
    
    To support the affinity setting of per cpu timers in the early startup
    of a not yet online cpu, implement the force logic, which disables the
    cpu online check.
    
    Tagged for stable to allow a simple fix of the affected SoC clock
    event drivers.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Krzysztof Kozlowski <k.kozlowski@samsung.com>
    Cc: Kyungmin Park <kyungmin.park@samsung.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Cc: Tomasz Figa <t.figa@samsung.com>,
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>,
    Cc: Kukjin Kim <kgene.kim@samsung.com>
    Cc: linux-arm-kernel@lists.infradead.org,
    Link: http://lkml.kernel.org/r/20140416143315.916984416@linutronix.de
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -246,8 +246,7 @@ static int gic_set_affinity(struct irq_data *d, const struct cpumask *mask_val,
 			    bool force)
 {
 	void __iomem *reg = gic_dist_base(d) + GIC_DIST_TARGET + (gic_irq(d) & ~3);
-	unsigned int shift = (gic_irq(d) % 4) * 8;
-	unsigned int cpu;
+	unsigned int cpu, shift = (gic_irq(d) % 4) * 8;
 	u32 val, mask, bit;
 
 	raw_spin_lock(&irq_controller_lock);
@@ -265,7 +264,10 @@ static int gic_set_affinity(struct irq_data *d, const struct cpumask *mask_val,
 		}
 		bit <<= shift;
 	} else {
-		cpu = cpumask_any_and(mask_val, cpu_online_mask);
+		if (!force)
+			cpu = cpumask_any_and(mask_val, cpu_online_mask);
+		else
+			cpu = cpumask_first(mask_val);
 
 		if (cpu >= NR_GIC_CPU_IF || cpu >= nr_cpu_ids)
 			goto err_out;
